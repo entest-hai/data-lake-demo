0d382d43205932cbaf0e9e499cabfc028c102cdc
entest <hai@entst.io>
2023-03-10 10:28:37 +0000

    test

--- config.ts
+++
@@ -1,9 +0,0 @@
-export const config = {
-  registerBucketData: "arn:aws:s3:::haimtran-codepipeline-artifact/data/",
-  dataLocationBucket: "haimtran-codepipeline-artifact",
-  athenaResultBucketArn:
-    "arn:aws:s3:::haimtran-codepipeline-artifact/data-analys/*",
-  dataAnalystArn: "arn:aws:iam::392194582387:user/DataAnalyst",
-  dataScientistArn: "arn:aws:iam::392194582387:user/DataScientist",
-  soureBucket: "haimtran-codepipeline-artifact",
-};
