fcbaca088754365723fa75483c167f26ab288220
entest <hai@entst.io>
2023-03-19 06:00:16 +0000

    update user-data

--- script/user_data.sh
+++ script/user_data.sh
@@ -5,11 +5,11 @@ yum install unzip
 yum install -y mariadb
 wget https://downloads.mysql.com/docs/sakila-db.zip . 
 unzip sakila-db.zip
-cdk sakila-db
+cd sakila-db
 touch run.sh 
 echo 'export host=' >> run.sh
 echo 'export user=demo' >> run.sh
 echo 'export password=' >> run.sh
-echo 'mysql --host=$host --user=$user --password=$password -f > sakila-schema.sql'
-echo 'mysql --host=$host --user=$user --password=$password -f > sakila-data.sql'
+echo 'mysql --host=$host --user=$user --password=$password -f > sakila-schema.sql' >> run.sh 
+echo 'mysql --host=$host --user=$user --password=$password -f > sakila-data.sql' >> run.sh 
 chmod 700 run.sh 
